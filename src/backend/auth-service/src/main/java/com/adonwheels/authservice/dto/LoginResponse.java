92eb6b35856129d73cf9fa3f2b18d904eda2d3f1
LukasHellesch <lukas.hellesch@gmail.com>
2025-08-14 23:49:18 +0200

    changes again to same response of the login

--- src/backend/auth-service/src/main/java/com/adonwheels/authservice/dto/LoginResponse.java
+++ src/backend/auth-service/src/main/java/com/adonwheels/authservice/dto/LoginResponse.java
@@ -6,11 +6,9 @@ public class LoginResponse {
     public LoginResponse(String token) {
         this.token = token;
     }
-
     public String getToken() {
         return token;
     }
-
     public void setToken(String token) {
         this.token = token;
     }
