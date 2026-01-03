91a045b28c684ae99d4653d168239d9b9aacbd0c
hellescl <lukas.hellesch@gmail.com>
2026-01-03 23:38:51 +0100

    remove unused imports

--- src/backend/auth-service/src/main/java/com/adonwheels/authservice/exception/RegistrationException.java
+++ src/backend/auth-service/src/main/java/com/adonwheels/authservice/exception/RegistrationException.java
@@ -1,8 +1,5 @@
 package com.adonwheels.authservice.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 // A base exception for registration failures
 public class RegistrationException extends RuntimeException {
     public RegistrationException(String message) {
