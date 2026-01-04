710bb89f05834e1bfbbc9a2c90271e52417405fa
hellescl <lukas.hellesch@gmail.com>
2026-01-04 14:48:54 +0100

    delete specific exceptions

--- src/backend/auth-service/src/main/java/com/adonwheels/authservice/exception/RegistrationException.java
+++
@@ -1,8 +0,0 @@
-package com.adonwheels.authservice.exception;
-
-// A base exception for registration failures
-public class RegistrationException extends RuntimeException {
-    public RegistrationException(String message) {
-        super(message);
-    }
-}
--- src/backend/auth-service/src/main/java/com/adonwheels/authservice/exception/RegistrationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.adonwheels.authservice.exception;
-
-public class RegistrationFailedException extends RuntimeException {
-    public RegistrationFailedException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
