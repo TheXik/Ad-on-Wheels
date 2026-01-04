3edc13ab8e06ecfba15bd79da8a369d701002ac9
hellescl <lukas.hellesch@gmail.com>
2026-01-04 14:48:54 +0100

    delete specific exceptions

--- src/backend/auth-service/src/main/java/com/adonwheels/authservice/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.adonwheels.authservice.exception;
-
-public class EmailAlreadyExistsException extends RegistrationException {
-    public EmailAlreadyExistsException(String email) {
-        super("This email address is already in use: " + email);
-    }
-}
--- src/backend/auth-service/src/main/java/com/adonwheels/authservice/exception/InvalidRegistrationRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.adonwheels.authservice.exception;
-
-public class InvalidRegistrationRequestException extends RegistrationException {
-    public InvalidRegistrationRequestException(String message) {
-        super(message);
-    }
-}
