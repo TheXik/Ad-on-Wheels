500be2a5d60ca4bfc9d2ca85f1a9134f54248c90
Lukas <lukas.hellesch@gmail.com>
2026-04-10 00:01:35 +0200

    tighten gateway open endpoints

--- src/backend/gateway-service/src/main/java/com/adonwheels/gatewayservice/filter/RouteValidator.java
+++ src/backend/gateway-service/src/main/java/com/adonwheels/gatewayservice/filter/RouteValidator.java
@@ -13,17 +13,11 @@ public class RouteValidator {
             "/auth/register",
             "/auth/login",
             "/auth/google",
-            "/auth/forgot-password",
-            "/auth/reset-password",
-            "/auth/send-verification",
-            "/auth/verify-email",
-            "/eureka", //TODO MAKE sure that this is okay to have it here  im not sure i know i need it for eureka discovery
-            //TODO but i dont know if it wont make issues with security
             "/campaigns/images/"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
             request -> openApiEndpoints
                     .stream()
-                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+                    .noneMatch(uri -> request.getURI().getPath().startsWith(uri));
 }
