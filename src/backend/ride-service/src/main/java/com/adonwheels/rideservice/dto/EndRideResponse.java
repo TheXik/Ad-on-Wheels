fbf89446cdd6d2838c5cbef0b245e5bf25aff51a
Lukas <lukas.hellesch@gmail.com>
2026-04-18 22:33:44 +0200

    feat: add ride verify endpoint

--- src/backend/ride-service/src/main/java/com/adonwheels/rideservice/dto/EndRideResponse.java
+++ src/backend/ride-service/src/main/java/com/adonwheels/rideservice/dto/EndRideResponse.java
@@ -2,12 +2,18 @@ package com.adonwheels.rideservice.dto;
 
 public class EndRideResponse {
 
+    private final Long completedRideId;
     private final double totalDistanceKm;
     private final long durationSeconds;
 
-    public EndRideResponse(double totalDistanceKm, long durationSeconds) {
+    public EndRideResponse(Long completedRideId, double totalDistanceKm, long durationSeconds) {
+        this.completedRideId = completedRideId;
         this.totalDistanceKm = totalDistanceKm;
         this.durationSeconds = durationSeconds;
+    }
+
+    public Long getCompletedRideId() {
+        return completedRideId;
     }
 
     public double getTotalDistanceKm() {
