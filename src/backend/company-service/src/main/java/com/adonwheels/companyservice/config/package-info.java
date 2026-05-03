f8d18c96078e6c6ff5d1341b657369812625c964
Lukas <lukas.hellesch@gmail.com>
2026-05-03 03:49:25 +0200

    feat: company-service config dto packages

---
+++ src/backend/company-service/src/main/java/com/adonwheels/companyservice/config/package-info.java
@@ -0,0 +1,3 @@
+// Reserved for company-service local configuration beans. Currently empty;
+// auto-configuration covers everything the service needs.
+package com.adonwheels.companyservice.config;
