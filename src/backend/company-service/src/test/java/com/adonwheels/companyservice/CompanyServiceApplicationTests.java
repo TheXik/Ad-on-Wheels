eb7be40e5955dde5e77f42ba5673ac1b3c264f90
Lukas <lukas.hellesch@gmail.com>
2026-05-03 03:48:15 +0200

    test: env-guard mysql contextLoads

--- src/backend/company-service/src/test/java/com/adonwheels/companyservice/CompanyServiceApplicationTests.java
+++ src/backend/company-service/src/test/java/com/adonwheels/companyservice/CompanyServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.adonwheels.companyservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledIfEnvironmentVariable(named = "RUN_CONTEXT_LOADS", matches = "true")
 class CompanyServiceApplicationTests {
 
     @Test
